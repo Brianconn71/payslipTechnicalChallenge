a5cd0bb1cb92a4f5560543fd644504e32da4ad08
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-04 16:29:31 +0000

    add: added unittests to check for line breaks in GTN file

---
+++ test_GTN_file_contains_line_breaks/test_GTN_file_contains_line_breaks_pass.py
@@ -0,0 +1,26 @@
+import unittest
+import pandas as pd
+
+# to make pandas work properly I had to install openpyxl
+
+
+class MyLineBreakTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.GTN_df = pd.read_excel("pass_data/GTN.xlsx")
+
+    def tearDown(self):
+        self.GTN_df = ""
+
+    def test_GTN_file_with_no_line_breaks(self):
+        dropped_line_breaks = self.GTN_df.dropna(how="all")
+        self.assertEqual(len(dropped_line_breaks), len(self.GTN_df), "GNT file has lineBreaks")
+
+    def test_GTN_file_with_line_breaks(self):
+        self.GTN_df_with_line_breaks = pd.read_excel("fail_data/GTNHasLineBreaks.xlsx")
+        dropped_line_breaks = self.GTN_df_with_line_breaks.dropna(how="all")
+        self.assertNotEqual(len(dropped_line_breaks), len(self.GTN_df_with_line_breaks), "GNT file has lineBreaks")
+
+
+if __name__ == '__main__':
+    unittest.main()
