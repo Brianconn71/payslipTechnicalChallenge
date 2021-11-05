f00473a04bd60e6c138cdd10f665d6e820a3e448
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-05 09:49:36 +0000

    edit: changed code in test employees are in GTN and not payrun to comply with pep 8

--- README.md
+++ README.md
@@ -3,33 +3,33 @@
 
 ## Challenge 1
 
-#### File is of type excel
+### File is of type excel
 
 ## Challenge 2
 
-#### GTN file contains line breaks
+### GTN file contains line breaks
 
 ## Challenge 3
 
-#### GTN file header structure has changed
+### GTN file header structure has changed
 
 ## Challenge 4
 
-#### Employees present in Payrun and missing in GTN
+### Employees present in Payrun and missing in GTN
 
 ## Challenge 5
 
-#### Employees present in GTN and missing in Payrun
+### Employees present in GTN and missing in Payrun
 
 ## Challenge 6
 
-#### Pay elements in GTN file do not map to Payrun file
+### Pay elements in GTN file do not map to Payrun file
 
 ## Challenge 7
 
-#### Pay elements in Payrun file do not map to GTN file
+### Pay elements in Payrun file do not map to GTN file
 
 ## Challenge 8
 
-#### Pay elements in GTN file are numeric
+### Pay elements in GTN file are numeric
 
--- test_employees_present_in_payrun_and_missing_in_GTN/test_employees_present_in_payrun_and_missing_in_GTN_pass.py
+++ test_employees_present_in_payrun_and_missing_in_GTN/test_employees_present_in_payrun_and_missing_in_GTN_pass.py
@@ -6,14 +6,20 @@ class MyTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.payrun_employee_Series = pd.read_excel("pass_data/Payrun.xlsx", skiprows=[1], usecols=["Employee ID"], squeeze=True)
-        cls.GTN_employee_Series = pd.read_excel("fail_data/GTNWrongEmployeeId.xlsx", usecols=["employee_id"], squeeze=True)
+        cls.payrun_series = pd.read_excel("pass_data/Payrun.xlsx",
+                                          skiprows=[1],
+                                          usecols=["Employee ID"],
+                                          squeeze=True)
+        cls.GTN_series = pd.read_excel("fail_data/GTNWrongEmployeeId.xlsx",
+                                       usecols=["employee_id"],
+                                       squeeze=True)
 
     def test_employees_in_payrun_and_missing_in_GTN(self):
-        payrun_employee_Series_cleaned = self.payrun_employee_Series.dropna()
-        payrun_employee_Series_reformated = payrun_employee_Series_cleaned.astype("int64")
-        series_match = payrun_employee_Series_reformated.equals(self.GTN_employee_Series)
-        self.assertFalse(series_match, "The Employees are present in Payrun AND in GTN file")
+        payrun_series_cleaned = self.payrun_series.dropna()
+        payrun_series_reformated = payrun_series_cleaned.astype("int")
+        series_match = payrun_series_reformated.equals(self.GTN_series)
+        self.assertFalse(series_match,
+                         "The Employees are present in Payrun AND in GTN file")
 
 
 if __name__ == '__main__':
