0372f5c53958c6c51855fac76b57c5d1ea829bf9
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-05 00:32:11 +0000

    edit: corrected some errors with unittests and made file_type_excel pep8 compliant

--- test_file_type_is_excel/test_file_type_is_excel_fail.py
+++ test_file_type_is_excel/test_file_type_is_excel_fail.py
@@ -5,28 +5,36 @@ import pathlib
 class MyFileTypeTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.GTN_file_extension = pathlib.Path("fail_data/fail_test.txt").suffix
-        self.payslip_format_file_extension = pathlib.Path("fail_data/fail_test.txt").suffix
+        self.GTN_file_ext = pathlib.Path("fail_data/fail_test.txt").suffix
+        self.payslip_file_ext = pathlib.Path("fail_data/fail_test.txt").suffix
 
     def tearDown(self):
-        self.GTN_file_extension = ""
-        self.payslip_format_file_extension = ""
+        self.GTN_file_ext = ""
+        self.payslip_file_ext = ""
 
     def test_check_both_files_are_excel_files(self):
-        self.assertEqual(self.GTN_file_extension, self.payslip_format_file_extension, "Both files are not in .xlsx format")
+        self.assertNotEqual(self.GTN_file_ext,
+                            self.payslip_file_ext,
+                            "Both files are in .xlsx format")
 
     def test_check_GTN_file_is_not_a_excel_file(self):
-        self.GTN_file_extension = pathlib.Path("fail_data/fail_test.txt").suffix
-        self.assertNotEqual(self.GTN_file_extension, self.payslip_format_file_extension, "The GTN file is not in .xlsx format")
+        self.GTN_file_ext = pathlib.Path("fail_data/fail_test.txt").suffix
+        self.assertNotEqual(self.GTN_file_ext,
+                            self.payslip_file_ext,
+                            "The GTN file is in .xlsx format")
 
     def test_check_payslip_file_is_not_a_excel_file(self):
-        self.payslip_format_file_extension = pathlib.Path("fail_data/fail_test.txt").suffix
-        self.assertNotEqual(self.GTN_file_extension, self.payslip_format_file_extension, "The payslip file is not in .xlsx format")
+        self.payslip_file_ext = pathlib.Path("fail_data/fail_test.txt").suffix
+        self.assertNotEqual(self.GTN_file_ext,
+                            self.payslip_file_ext,
+                            "The payrun file is in .xlsx format")
 
     def test_check_both_files_are_not_excel_files(self):
-        self.GTN_file_extension = pathlib.Path("fail_data/fail_test.txt").suffix
-        self.payslip_format_file_extension = pathlib.Path("fail_data/fail_test.txt").suffix
-        self.assertNotEqual(self.GTN_file_extension, self.payslip_format_file_extension, "both files are not is .xlsx format")
+        self.GTN_file_ext = pathlib.Path("fail_data/fail_test.txt").suffix
+        self.payslip_file_ext = pathlib.Path("fail_data/fail_test.txt").suffix
+        self.assertNotEqual(self.GTN_file_ext,
+                            self.payslip_file_ext,
+                            "Both files are not in .xlsx format")
 
 
 if __name__ == '__main__':
--- test_file_type_is_excel/test_file_type_is_excel_pass.py
+++ test_file_type_is_excel/test_file_type_is_excel_pass.py
@@ -3,30 +3,44 @@ import pathlib
 
 
 class MyFileTypeTestCase(unittest.TestCase):
+    """
+    The below tests check if the file types are in .xlsx format
+    """
 
     def setUp(self):
-        self.GTN_file_extension = pathlib.Path("pass_data/GTN.xlsx").suffix
-        self.payslip_format_file_extension = pathlib.Path("pass_data/Payrun.xlsx").suffix
+        self.GTN_file_ext = pathlib.Path("pass_data/GTN.xlsx").suffix
+        self.payrun_file_ext = pathlib.Path(
+                                            "pass_data/Payrun.xlsx").suffix
 
     def tearDown(self):
-        self.GTN_file_extension = ""
-        self.payslip_format_file_extension = ""
+        self.GTN_file_ext = ""
+        self.payrun_file_ext = ""
 
     def test_check_both_files_are_excel_files(self):
-        self.assertEqual(self.GTN_file_extension, self.payslip_format_file_extension, "Both files are in .xlsx format")
+        self.assertEqual(self.GTN_file_ext,
+                         self.payrun_file_ext,
+                         "Both files are not in .xlsx format")
 
     def test_check_GTN_file_is_not_a_excel_file(self):
-        self.GTN_file_extension = pathlib.Path("fail_data/fail_test.txt").suffix
-        self.assertNotEqual(self.GTN_file_extension, self.payslip_format_file_extension, "The GNT file is not in .xlsx format")
+        self.GTN_file_ext = pathlib.Path("fail_data/fail_test.txt").suffix
+        self.assertNotEqual(self.GTN_file_ext,
+                            self.payrun_file_ext,
+                            "The GTN file is in .xlsx format")
 
     def test_check_payrun_file_is_not_a_execl_file(self):
-        self.payslip_format_file_extension = pathlib.Path("fail_data/fail_test.txt").suffix
-        self.assertNotEqual(self.GTN_file_extension, self.payslip_format_file_extension, "The payrun file is not in .xlsx format")
+        self.payrun_file_ext = pathlib.Path("fail_data/fail_test.txt").suffix
+        self.assertNotEqual(self.GTN_file_ext,
+                            self.payrun_file_ext,
+                            "The payrun file is in .xlsx format")
 
-    def test_check_both_files_are_not_execl_files(self):
-        self.GTN_file_extension = pathlib.Path("fail_data/fail_test.txt").suffix
-        self.payslip_format_file_extension = pathlib.Path("fail_data/fail_test.txt").suffix
-        self.assertNotEqual(self.GTN_file_extension, self.payslip_format_file_extension, "both files are not is .xlsx format")
+    def test_check_both_files_are_not_excel_files(self):
+        self.GTN_file_ext = pathlib.Path(
+                                         "fail_data/fail_test.txt").suffix
+        self.payrun_file_ext = pathlib.Path(
+                                            "fail_data/fail_test.txt").suffix
+        self.assertEqual(self.GTN_file_ext,
+                         self.payrun_file_ext,
+                         "Both files are not is .xlsx format")
 
 
 if __name__ == '__main__':
