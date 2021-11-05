8e83b4ce4033d0a44317eba9913b77f70f2871a2
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-05 00:49:05 +0000

    edit: changed unit tests in contains_line_breaks to adhere to pep8 compliance

--- test_GTN_file_contains_line_breaks/test_GTN_file_contains_line_breaks_fail.py
+++ test_GTN_file_contains_line_breaks/test_GTN_file_contains_line_breaks_fail.py
@@ -5,21 +5,28 @@ import pandas as pd
 
 
 class MyLineBreakTestCase(unittest.TestCase):
+    """
+    The below tests the GTN file has line breaks
+    """
 
     def setUp(self):
-        self.GTN_df = pd.read_excel("fail_data/GTNHasLineBreaks.xlsx")
+        self.GTN_df = pd.read_excel("fail_data/GTNLineBreaks.xlsx")
 
     def tearDown(self):
         self.GTN_df = ""
 
     def test_GTN_file_with_no_line_breaks(self):
         dropped_line_breaks = self.GTN_df.dropna(how="all")
-        self.assertEqual(len(dropped_line_breaks), len(self.GTN_df), "GNT file has lineBreaks")
+        self.assertEqual(len(dropped_line_breaks),
+                         len(self.GTN_df),
+                         "GNT file has lineBreaks")
 
     def test_GTN_file_with_line_breaks(self):
         self.GTN_df_with_line_breaks = pd.read_excel("pass_data/GTN.xlsx")
         dropped_line_breaks = self.GTN_df_with_line_breaks.dropna(how="all")
-        self.assertNotEqual(len(dropped_line_breaks), len(self.GTN_df_with_line_breaks), "GNT file has lineBreaks")
+        self.assertNotEqual(len(dropped_line_breaks),
+                            len(self.GTN_df_with_line_breaks),
+                            "GNT file has lineBreaks")
 
 
 if __name__ == '__main__':
--- test_GTN_file_contains_line_breaks/test_GTN_file_contains_line_breaks_pass.py
+++ test_GTN_file_contains_line_breaks/test_GTN_file_contains_line_breaks_pass.py
@@ -5,6 +5,9 @@ import pandas as pd
 
 
 class MyLineBreakTestCase(unittest.TestCase):
+    """
+    The below tests the GTN file has line breaks
+    """
 
     def setUp(self):
         self.GTN_df = pd.read_excel("pass_data/GTN.xlsx")
@@ -14,12 +17,16 @@ class MyLineBreakTestCase(unittest.TestCase):
 
     def test_GTN_file_with_no_line_breaks(self):
         dropped_line_breaks = self.GTN_df.dropna(how="all")
-        self.assertEqual(len(dropped_line_breaks), len(self.GTN_df), "GNT file has lineBreaks")
+        self.assertEqual(len(dropped_line_breaks),
+                         len(self.GTN_df),
+                         "GTN file has lineBreaks")
 
     def test_GTN_file_with_line_breaks(self):
-        self.GTN_df_with_line_breaks = pd.read_excel("fail_data/GTNHasLineBreaks.xlsx")
-        dropped_line_breaks = self.GTN_df_with_line_breaks.dropna(how="all")
-        self.assertNotEqual(len(dropped_line_breaks), len(self.GTN_df_with_line_breaks), "GNT file has lineBreaks")
+        self.GTN_df_has_breaks = pd.read_excel("fail_data/GTNLineBreaks.xlsx")
+        dropped_line_breaks = self.GTN_df_has_breaks.dropna(how="all")
+        self.assertNotEqual(len(dropped_line_breaks),
+                            len(self.GTN_df_has_breaks),
+                            "GTN file has lineBreaks")
 
 
 if __name__ == '__main__':
